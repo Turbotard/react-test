2f497b6fcc259831a609a92574481eb398eaf982
Turbotard <turbotard9@gmail.com>
2023-10-23 15:22:04 +0200

    tentative raté de login avec firebase

---
+++ src/db/db.tsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCvSwOdiRr37Ir2oEdogGpOEs0gIJsF3ms",
+  authDomain: "react-54d8b.firebaseapp.com",
+  projectId: "react-54d8b",
+  storageBucket: "react-54d8b.appspot.com",
+  messagingSenderId: "1044103979617",
+  appId: "1:1044103979617:web:299a1947472bcbebbd0020",
+  measurementId: "G-M27DYXL638"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
