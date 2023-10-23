3bab48c6a0b63d08542c24abffd3e270262173f6
Turbotard <turbotard9@gmail.com>
2023-10-23 10:08:03 +0200

    exo 1 (ajotuer une tache)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,35 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders logo', () => {
+  render(<App />);
+  const logoElement = screen.getByAltText('logo');
+  expect(logoElement).toBeInTheDocument();
+});
+
+test('renders header', () => {
+  render(<App />);
+  const headerElement = screen.getByRole('heading', { name: /edit src\/App\.tsx and save to reload\./i });
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('renders paragraph', () => {
+  render(<App />);
+  const paragraphElement = screen.getByText(/edit src\/App\.tsx and save to reload\./i);
+  expect(paragraphElement).toBeInTheDocument();
+});
+
+test('renders link to React website', () => {
+  render(<App />);
+  const reactLinkElement = screen.getByRole('link', { name: /learn react/i });
+  expect(reactLinkElement).toHaveAttribute('href', 'https://reactjs.org');
+  expect(reactLinkElement).toHaveAttribute('target', '_blank');
+  expect(reactLinkElement).toHaveAttribute('rel', 'noopener noreferrer');
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+interface Task {
+  task: string;
+  time: string;
+}
+
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState<string>('');
+  const [newTime, setNewTime] = useState<string>('');
+
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleNewTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleNewTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTime(event.target.value);
+  };
+
+  const handleNewTaskSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setTasks([...tasks, { task: newTask, time: newTime }]);
+    setNewTask('');
+    setNewTime('');
+  };
+
+  const handleTaskDelete = (index: number) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>To-Do List</h1>
+        <form onSubmit={handleNewTaskSubmit}>
+          <label>
+            Task:
+            <input type="text" value={newTask} onChange={handleNewTaskChange} style={{ marginRight: '10px', padding: '5px' }} />
+          </label>
+          <label>
+            Time:
+            <input type="text" value={newTime} onChange={handleNewTimeChange} style={{ marginRight: '10px', padding: '5px' }} />
+          </label>
+          <button type="submit" style={{ padding: '5px 10px', backgroundColor: 'green', color: 'white', border: 'none' }}>Add Task</button>
+        </form>
+        <h2>Tasks:</h2>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <strong>{task.time}:</strong> {task.task}
+              <button onClick={() => handleTaskDelete(index)} style={{ marginLeft: '10px', padding: '5px', backgroundColor: 'red', color: 'white', border: 'none' }}>Delete</button>
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
